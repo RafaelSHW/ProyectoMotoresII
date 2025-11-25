67d1a12423ab81ad1678bac263a8059408a419ac
RafaelSHW <1hrafita@gmail.com>
2025-11-25 08:46:59 -0800

    Muerte, marcos Menu

--- Assets/Scripts/KillBox.cs
+++ Assets/Scripts/KillBox.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class KillBox : MonoBehaviour
 {
     public GameObject player;
     public Transform repawn;
     public int nivel;
+    public Animator playerAnim;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
             //alternativa 1
-            player.transform.position = repawn.position;
+            
 
             //alternativa 2 y reasignar despues a boton de retry
             //Scene reinicioNilvel = SceneManager.GetActiveScene();
             //SceneManager.LoadScene(reinicioNilvel.name);
         }
+    }
+    IEnumerator Dead()
+    {
+        playerAnim.SetBool("Muerte", true);
+
+        yield return new WaitForSeconds(1f);       
+        player.transform.position = repawn.position;
+        playerAnim.SetBool("Muerte", false);
+        playerAnim.SetFloat("Camina", 0f);
     }
 }
